728d09ed171a6c5708a8f6a23f4a6dc0c1a233bd
Wandzfuture <michael.zabas@gmail.com>
2024-08-22 12:05:29 -0700

    0x02. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,13 +3,12 @@
 Module for calculating minimum operations to achieve n 'H' characters
 """
 
+
 def minOperations(n):
     """
     Calculates the fewest number of operations to result in n 'H' characters
-
     Args:
         n (int): The desired number of 'H' characters.
-
     Returns:
         int: The minimum number of operations. Returns 0 if can't be achieved
     """
